e79913394a3be81bb03c21ec742f2ddd248168d7
Ana Oliveira <theanaoliveira@outlook.com>
2020-08-03 12:23:45 -0300

    Included healthcheck

---
+++ csharp-demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
